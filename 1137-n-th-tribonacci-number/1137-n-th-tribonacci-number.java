4b2b62fc85e7c5b0cc8425055d036e595743291d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-02 00:24:03 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.97%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -6,8 +6,8 @@ class Solution {
         if(n == 2){
             return 1;
         }
-
-        int [] dp = new int[n+1];
+ 
+         int [] dp = new int[n+1];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
