3d56881f80f37e3a077b3b8902fd7ff15d2b626b
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-05 21:28:45 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.29%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,37 +1,49 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack <Character> s1 = new Stack<>();
-        Stack <Character> s2 = new Stack<>();
+        int i =s.length() -1;
+        int j =t.length() -1;
 
-        for(int i =0; i<s.length(); i++){
-           if(s1.isEmpty() && s.charAt(i) == '#'){
-           continue;
-           }
-           else if (s.charAt(i) == '#'){
-             s1.pop();
-           }
-           else{
-            s1.push(s.charAt(i));
-           }
-        }
-        for(int i =0; i<t.length(); i++){
-              if(s2.isEmpty() && t.charAt(i) == '#'){
-            continue;
-           }
-           else if(t.charAt(i) == '#'){
-           s2.pop();
-           }
-           else{
-             s2.push(t.charAt(i));
-           }
-        }
-        while(!s1.isEmpty() && !s2.isEmpty()){
-            if(s1.pop() != s2.pop()){
+        int skipi = 0;
+        int skipj = 0;
+
+        while(i>=0 || j >=0){
+            while(i>=0){
+                if(s.charAt(i) == '#'){
+                    skipi++;
+                    i--;
+                }
+                else if(skipi > 0){
+                    skipi--;
+                    i--;
+                }
+                else{
+                    break;
+                }
+            }
+
+             while(j>=0){
+                if(t.charAt(j) == '#'){
+                    skipj++;
+                    j--;
+                }
+                else if(skipj > 0){
+                    skipj--;
+                    j--;
+                }
+                else{
+                    break;
+                }
+            }
+             if (i >= 0 && j >= 0) {
+            if(s.charAt(i) != t.charAt(j)){
                 return false;
             }
-        }
-        if(!s1.isEmpty() || !s2.isEmpty()){
-            return false;
+            }
+            else if(i>=0 || j >=0){
+                return false;
+            }
+            i--;
+            j--;
         }
         return true;
     }
