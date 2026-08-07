23a64f0076f090c6bdfb0a998fd955d936ba0414
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-07 15:17:56 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.81%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,16 +1,15 @@
 class Solution {
     public int bitwiseComplement(int n) {
         if(n == 0) return 1;
-        int digit = (int) (Math.log(n)/Math.log(2))+1;
-
-        int []count = new int [digit];
-        for(int i=0 ; i<digit ; i++){
-            count[(digit-1) -i] = ((n>>i)&1)^1;
-        }
-        int ans = 0;
-        for(int j =0; j<digit ; j++){
-            ans = (ans<<1)|count[j];
-        }
-        return ans;
+        // int digit = (int) (Math.log(n)/Math.log(2))+1;
+        int mask =0;
+        int temp = n;
+       while(temp>0){
+        mask = (mask<<1)|1;
+        temp = temp>>1;
+       }
+        
+        
+        return n^mask;
     }
 }
