266f0890da9712db4135541f79a56ed2b442175f
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-08 00:50:03 +0530

    Time: 3 ms (29.39%), Space: 48.8 MB (23.47%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,34 +1,40 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
+
+        List<String> ans = new ArrayList<>();
+
+        if(digits.length() == 0)
+            return ans;
+
         String[] keypad = {
-    "",     // 0
-    "",     // 1
-    "abc",  // 2
-    "def",  // 3
-    "ghi",  // 4
-    "jkl",  // 5
-    "mno",  // 6
-    "pqrs", // 7
-    "tuv",  // 8
-    "wxyz"  // 9
-};
-String []arr = new String [digits.length()];
-for(int i=0; i<arr.length; i++){
-    arr[i]= keypad[digits.charAt(i)-'0'];
-}
-List <String> list = new ArrayList<>();
-    helper("",arr,list);
-    return list;
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        helper("", 0, digits, keypad, ans);
+
+        return ans;
     }
-    public void helper(String p,String[]key,List <String>list){
-        if(key.length == 0){
-            list.add(p);
+
+
+    private void helper(String p, int index, String digits,
+                        String[] keypad, List<String> ans) {
+
+        if(index == digits.length()) {
+            ans.add(p);
             return;
         }
-    String letter = key[0];
-    for(int i=0; i<letter.length(); i++){
-        char ch = letter.charAt(i);
-         helper(p+ch,Arrays.copyOfRange(key,1,key.length),list);
-    }
+
+        String letters = keypad[digits.charAt(index) - '0'];
+
+        for(int i = 0; i < letters.length(); i++) {
+            helper(
+                p + letters.charAt(i),
+                index + 1,
+                digits,
+                keypad,
+                ans
+            );
+        }
     }
 }
