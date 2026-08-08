6878efaa234fca75bd0fabcab36ac9a43b1b2373
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-08 12:37:12 +0530

    Time: 76 ms (34.12%), Space: 107.8 MB (23.26%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        int [] ans = new int [temp.length];
+        Stack <Integer> stack = new Stack<>();
+        stack.push(0);
+        for(int i=1; i<temp.length; i++){
+
+          while(!stack.isEmpty() && temp[stack.peek()] < temp[i]){
+            ans[stack.peek()] = i - stack.pop();
+          }
+          stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            ans[stack.pop()] = 0;
+        }
+        return ans;
+    }
+}
