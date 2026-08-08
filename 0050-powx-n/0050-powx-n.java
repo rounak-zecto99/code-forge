d175b07fb75be15c51816386549c6bbeaa0cee92
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-09 02:17:19 +0530

    Time: 0 ms (100%), Space: 48.3 MB (22.95%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,21 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-       long nn = n;
-       
-        if(nn<0){
-            nn = -1*nn;
-            return 1.0/helper(x,nn);
-        }
-        return helper(x,nn);
+     long nn = n;
+     if(nn<0){
+        nn = -1*nn;
+        return 1.0/dick(x,nn);
+     }
+     return dick(x,nn);
+     
     }
-    public double helper(double x , long n){
-
-         if(n == 0){
+    public double dick(double x  , long n ){
+           if(n == 0){
             return 1;
         }
-        if(n%2 == 0){
-            return helper(x*x,n/2);
-        }
-        return x*helper(x,n-1);
+      if (n%2==0){
+          return dick(x*x,n/2);
+      }
+      return x * dick(x,n-1);
     }
 }
