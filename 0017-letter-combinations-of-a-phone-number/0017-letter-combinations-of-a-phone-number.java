d7960792cf438557d928eb477c988c7d839072b3
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-14 01:30:53 +0530

    Time: 3 ms (29.56%), Space: 48.9 MB (21.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,40 +1,32 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-
-        List<String> ans = new ArrayList<>();
-
-        if(digits.length() == 0)
-            return ans;
-
-        String[] keypad = {
-            "", "", "abc", "def", "ghi",
-            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        List<String> list = new ArrayList<>();
+        String [] letter = {
+            "",
+            "",
+           "abc",
+           "def",
+           "ghi",
+           "jkl",
+           "mno",
+           "pqrs",
+           "tuv",
+           "wxyz"
         };
-
-        helper("", 0, digits, keypad, ans);
-
-        return ans;
+         helper(list, "" , letter, digits, 0);
+         return list;
     }
-
-
-    private void helper(String p, int index, String digits,
-                        String[] keypad, List<String> ans) {
-
-        if(index == digits.length()) {
-            ans.add(p);
+    public void helper( List<String> list, String s, String []letter, String digits, int index){
+          if(index == digits.length()){
+            list.add(s);
             return;
-        }
+          }
 
-        String letters = keypad[digits.charAt(index) - '0'];
+        String curr = letter[digits.charAt(index) - '0'] ;
 
-        for(int i = 0; i < letters.length(); i++) {
-            helper(
-                p + letters.charAt(i),
-                index + 1,
-                digits,
-                keypad,
-                ans
-            );
+        for(int i=0; i<curr.length(); i++){
+            char ch = curr.charAt(i);
+             helper(list, s+ch , letter, digits, index+1);
         }
     }
 }
