202e781bb087bb8c04253372746d576627b88a1d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-15 21:21:25 +0530

    Time: 2 ms (80.93%), Space: 46.6 MB (47.03%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> list = new ArrayList<>();
-        int[] queens = new int[n];
-        boolean[] col = new boolean[n];
+        boolean[] columns = new boolean[n];
         boolean[] diag1 = new boolean[2 * n - 1];
         boolean[] diag2 = new boolean[2 * n - 1];
+        int[] queens = new int[n];
 
-        helper(list, queens, col, diag1, diag2, 0, n);
+        helper(list, columns, diag1, diag2, queens, 0, n);
         return list;
     }
 
-    public void helper(List<List<String>> list, int[] queens, boolean[] col, boolean[] diag1,
-                       boolean[] diag2, int row, int n) {
+    public void helper(List<List<String>> list, boolean[] columns, boolean[] diag1, boolean[] diag2, int[] queens, int row, int n) {
         if (row == n) {
-            List<String> ans = new ArrayList<>(n);
+            List<String> ram = new ArrayList<>();
+
             for (int i = 0; i < n; i++) {
                 StringBuilder a = new StringBuilder();
                 for (int j = 0; j < n; j++) {
@@ -23,29 +23,26 @@ class Solution {
                     }
                     a.append(".");
                 }
-                ans.add(a.toString());
+                ram.add(a.toString());
             }
-            list.add(ans);
-            return;
+            list.add(ram);
         }
-
-
-        for (int column = 0; column < n; column++) {
-            if (col[column] || diag1[row + column] || diag2[row - column + n - 1]) {
+        for (int col = 0; col < n; col++) {
+            if (columns[col] || diag1[row + col] || diag2[row - col + n - 1]) {
                 continue;
             }
-            col[column] = true;
-            diag1[row + column] = true;
-            diag2[row - column + queens.length - 1] = true;
+            columns[col] = true;
+            diag1[row + col] = true;
+            diag2[row - col + n - 1] = true;
 
-            queens[row] = column;
+            queens[row] = col;
 
-            helper(list, queens, col, diag1, diag2, row+1, n);
+            helper(list, columns, diag1, diag2, queens, row + 1, n);
 
-            col[column] = false;
-            diag1[row + column] = false;
-            diag2[row - column + n - 1] = false;
+            columns[col] = false;
+            diag1[row + col] = false;
+            diag2[row - col + n - 1] = false;
+
         }
     }
-
 }
