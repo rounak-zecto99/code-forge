d3f38f1cd5f48fcc52cd4a8b84e6272d8a7f3cb1
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-16 17:22:07 +0530

    Time: 45 ms (92.72%), Space: 91.2 MB (24.97%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.java
@@ -6,21 +6,23 @@ class Solution {
         }
         long start = totalTrips / time.length;
         long end = (long) min * totalTrips;
-        long ans = 0;
-        while(start<=end){
+
+        while(start<end){
             long mid = start+(end-start)/2;
             long trips = 0;
             for(int i=0; i<time.length ; i++){
                 trips += mid/time[i];
+                if(trips >= totalTrips){
+                   break;
+                 }
             }
             if(trips<totalTrips){
                 start = mid+1;
             }
             else{
-                ans = mid;
-                end = mid -1;
+                end = mid;
             }
         }
-        return ans;
+        return start;
     }
 }
