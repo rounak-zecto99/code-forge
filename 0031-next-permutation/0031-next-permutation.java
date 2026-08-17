e6693ee9507a8f4a61e459104348b60e7ba7cf36
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-17 14:52:33 +0530

    Time: 2 ms (6.95%), Space: 45 MB (5.32%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void swap (int a, int b, int[] nums){
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+    public void reverse(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+
+    while (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+}
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+
+        for(int i=nums.length-2; i>=0; i--){
+            if(nums[i+1]>nums[i]){
+                index = i;
+                break;
+            }
+        }
+          if(index == -1){
+            reverse(nums);
+            return;
+        }
+        for(int j=nums.length-1; j >index; j--){
+            if(nums[j]>nums[index]){
+               swap(index,j,nums);
+                break;
+            }
+        }
+       Arrays.sort(nums,index+1,nums.length);
+        
+    }
+   
+}
