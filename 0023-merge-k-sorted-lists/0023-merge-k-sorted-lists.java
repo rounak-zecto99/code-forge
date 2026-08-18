8c714c12d4f1a90ecf62cf83d14bf2b8cda546b6
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-19 00:27:31 +0530

    Time: 4 ms (79.93%), Space: 46.8 MB (64.84%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,35 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-      PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
-       for(ListNode head:lists){
-        if(head!=null)
-        heap.offer(head);
-       }
+
+        PriorityQueue<ListNode> heap =
+                new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode head : lists) {
+            if (head != null) {
+                heap.offer(head);
+            }
+        }
+
         ListNode dummy = new ListNode(-1);
         ListNode tail = dummy;
 
-        while(!heap.isEmpty()){
-            ListNode dick = heap.poll();
+        while (!heap.isEmpty()) {
 
-            tail.next = dick;
+            ListNode node = heap.poll();
+
+            tail.next = node;
             tail = tail.next;
 
-            if(dick.next != null)
-            heap.offer(dick.next);
-
-            
+            if (node.next != null) {
+                heap.offer(node.next);
+            }
         }
+
         tail.next = null;
+
         return dummy.next;
     }
 }
