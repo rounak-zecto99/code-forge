94aa432c5e4f07ff399665c440175860aaa6d4d9
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-20 02:55:36 +0530

    Time: 2 ms (98.01%), Space: 72.7 MB (13.89%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -2,26 +2,36 @@ class Solution {
     public boolean lemonadeChange(int[] bills) {
         int five = 0;
         int ten = 0;
-        for (int num : bills) {
-            if (num == 5) {
+
+        for (int bill : bills) {
+
+            if (bill == 5) {
                 five++;
-            } else if (num == 10) {
+            }
+
+            else if (bill == 10) {
                 if (five == 0)
                     return false;
 
+                five--;
                 ten++;
-                five--;
-            }else{
-            if (ten > 0 && five > 0) {
-                ten--;
-                five--;
-            } else if (five >= 3) {
-                five -= 3;
-            } else {
-                return false;
+            }
+
+            else { // bill == 20
+
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3) {
+                    five -= 3;
+                }
+                else {
+                    return false;
+                }
             }
         }
-        }
+
         return true;
     }
 }
