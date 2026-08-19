7199db15688b4e913d4961da99fae8a36aea390d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-19 10:57:09 +0530

    Time: 1 ms (100%), Space: 48.3 MB (98.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,58 +1,58 @@
 class Solution {
-    public double findMedianSortedArrays(int[] a, int[] b) {
-        
-        if(b.length < a.length){
-            return findMedianSortedArrays(b,a);
-        }
-        int half = (a.length+b.length+1)/2;
-        int s=0; int e= a.length;
-            int left1;
-            int right1;
-            int left2;
-            int right2;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums2.length<nums1.length)
+        return findMedianSortedArrays(nums2,nums1);
+        int start = 0;
+        int end = nums1.length;
+        int left1;
+        int left2;
+        int right1;
+        int right2;
+        int half = (nums1.length + nums2.length+1)/2;
 
-        while(s<=e){
-            int cutA = s+(e-s)/2;
+        while(start<=end){
+            int cutA = start+(end-start)/2;
             int cutB = half - cutA;
 
             if(cutA == 0){
-                left1 =Integer.MIN_VALUE;
-            } else{
-                left1 = a[cutA-1];
+            left1 = Integer.MIN_VALUE;
             }
-            if(cutA == a.length){
-                right1 =Integer.MAX_VALUE;
-            } else{
-                right1 = a[cutA];
+            else{
+                left1 = nums1[cutA-1];
+            }
+            if(cutA == nums1.length){
+                right1 = Integer.MAX_VALUE;
+            }
+            else{
+                right1 = nums1[cutA];
             }
 
-              if(cutB == 0){
-                left2 =Integer.MIN_VALUE;
-            } else{
-                left2 = b[cutB-1];
+           if(cutB == 0){
+            left2 = Integer.MIN_VALUE;
             }
-            if(cutB == b.length){
-                right2 =Integer.MAX_VALUE;
-            } else{
-                right2 = b[cutB];
+            else{
+                left2 = nums2[cutB-1];
+            }
+            if(cutB == nums2.length){
+                right2 = Integer.MAX_VALUE;
+            }
+            else{
+                right2 = nums2[cutB];
             }
 
             if(left1<=right2 && left2<=right1){
-             if((a.length+b.length)%2 == 0){
-                double ans = Math.max(left1,left2) + Math.min(right1,right2);
-                return ans/2.0;
-             }
-           
-                return Math.max(left1,left2);
-              
+              if((nums1.length + nums2.length) %2 == 0){
+              double ans =Math.max(left1,left2) + Math.min(right1,right2);
+              return ans/2.0;
+              }
+              return(double) Math.max(left1,left2);
             }
-            else if(left1>right2){
-                e = cutA-1;
+            if(left1>right2){
+                end = cutA-1;
             }
-            else {
-                s = cutA+1;
+            else{
+                start = cutA+1;
             }
-
         }
         return 0.0;
     }
