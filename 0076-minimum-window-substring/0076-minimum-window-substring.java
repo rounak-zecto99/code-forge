465601970c2489c4035d84c009bb5a0825f6bb41
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-20 23:26:53 +0530

    Time: 24 ms (22.61%), Space: 47.8 MB (23.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,52 +4,50 @@ class Solution {
         //  for(int n : t.toCharArray()){
         //     freq[n-'A']++;
         //  }
-        HashMap <Character,Integer> need = new HashMap<>();
-        HashMap <Character,Integer> have = new HashMap<>();
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> have = new HashMap<>();
 
-        for(char n: t.toCharArray()){
-            need.put(n, need.getOrDefault(n,0)+1);
+        for (char n : t.toCharArray()) {
+            need.put(n, need.getOrDefault(n, 0) + 1);
         }
-        int start =-1;
-        int end =s.length();
+        int start = -1;
+        int end = s.length();
         int left = 0;
-        int size =0;
+        int size = 0;
         int req = t.length();
-    
-    for(int right=0; right<s.length(); right++){
-        char ch = s.charAt(right);
 
-        if(need.containsKey(ch)){
-        have.put(ch,have.getOrDefault(ch,0)+1);
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
 
-        if(have.get(ch)<=need.get(ch))
-        req --;
+            if (need.containsKey(ch)) {
+                have.put(ch, have.getOrDefault(ch, 0) + 1);
+
+                if (have.get(ch) <= need.get(ch))
+                    req--;
+            }
+            while (req == 0) {
+                if (start == -1 || right - left < end - start) {
+                    start = left;
+                    end = right;
+                }
+
+                char back = s.charAt(left);
+
+                if (!need.containsKey(back)) {
+                    left++;
+                } else if (have.get(back) > need.get(back)) {
+                    have.put(back, have.get(back) - 1);
+                    left++;
+                } else {
+                    break;
+                }
+            }
+
         }
-        while(req == 0){
-         char back = s.charAt(left);
-         if(!need.containsKey(back)){
-            left++;
-         }
-         else if(have.get(back)>need.get(back)){
-            have.put(back,have.get(back)-1);
-            left++;
-         }
-         else{
-            if (start == -1 || right - left < end - start) {
-                        start = left;
-                        end = right;
-                    }
-            break;
-         }
+            if (start != -1)
+                return s.substring(start, end + 1);
+
+            return "";
+
         }
-
     }
-   
-
-    if(start != -1)
-    return s.substring(start,end+1);
-
-    return "";
-
-    }
-}
