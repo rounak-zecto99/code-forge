0eec8d09414a0dc5c6571547a885cbcf945a6cd9
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-20 17:51:31 +0530

    Time: 1 ms (99.96%), Space: 94.4 MB (71.58%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left =0;
+        int right = -1;
+        int profit=0;
+
+        while(++right<prices.length){
+            if(prices[right]>prices[left]){
+                profit = Math.max(profit,prices[right]-prices[left]);
+            }
+            else{
+                left = right;
+            }
+            
+        }
+        return profit;
+    }
+}
