e94d5e0862986fe40b5fcc3f1321d0e85dc8cd9a
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-20 15:49:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,27 +1,20 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int max = 0;
-        int left =0;
+        HashMap <Integer, Integer> map = new HashMap<>();
+        int left = 0;
+        int basket = 0;
 
-        for(int right=0; right<fruits.length; right++){
-            if(map.containsKey(fruits[right])){
-                map.put(fruits[right],map.get(fruits[right])+1);
-            }
-            else{
-                map.put(fruits[right],1);
-            }
+        for(int i=0; i<fruits.length; i++){
+            map.put(fruits[i],i);
 
-            while(map.size()>2){
-                int leftmost = fruits[left++];
-               map.put(leftmost,map.get(leftmost)-1);
-
-               if(map.get(leftmost) == 0){
-                map.remove(leftmost);
-               }
+            if(map.size()>2){
+                int fruit = fruits[left];
+                left = Math.max(left,map.get(fruit)+1);
+                map.remove(fruit);
+                
             }
-            max = Math.max(max, right - left +1);
+            basket = Math.max(basket,i-left+1);
         }
-        return max;
+        return basket;
     }
 }
