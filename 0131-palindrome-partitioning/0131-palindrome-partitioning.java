860c5d7fe585192f283abfb4981da081124c0ae9
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-21 21:51:17 +0530

    Time: 8 ms (74.12%), Space: 65.3 MB (58.46%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,36 +1,35 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> list = new ArrayList<>();
-         helper(list,new ArrayList<>(),s,0);
-         return list;
-    }
-    public void helper(List<List<String>> list, List<String> curr,String s, int index){
-        if(index == s.length()){
-            list.add(new ArrayList<>(curr));
-            return;
-        }
-
-        for(int i= index; i<s.length(); i++){
-             String part = s.substring(index, i + 1);
-              if(pall(part)){
-                curr.add(part);
-                helper(list,curr,s,i+1);
-                curr.remove(curr.size()-1);
-              }
-        }
-    }
-    public boolean pall(String s){
-        char [] arr = s.toCharArray();
+    public boolean isVal(String s){
         int left = 0;
-        int right = arr.length-1;
+        int right = s.length()-1;
 
         while(left<right){
-            if(arr[left] != arr [right]){
-                return false;
+            if(s.charAt(left) != s.charAt(right)){
+            return false;
             }
             left++;
             right--;
         }
         return true;
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> list = new ArrayList<>();
+       helper(list, new ArrayList<>(), 0 ,s);
+        return list;
+    }
+    public void helper(List<List<String>> list, List<String> row, int index, String s){
+        if(index == s.length()){
+            list.add(new ArrayList<>(row));
+            return;
+        }
+
+        for(int i=index; i<s.length() ; i++){
+            String cur = s.substring(index,i+1);
+            if(isVal(cur)){
+                row.add(cur);
+                helper(list, row, i+1 ,s);
+                row.remove(row.size()-1);
+            }
+        }
     }
 }
