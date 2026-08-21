01048631652f4d47501c83b4dcacfdaba0515c74
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 00:23:57 +0530

    Time: 59 ms (43.57%), Space: 82.3 MB (8.3%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,53 +1,50 @@
 class Solution {
-     public boolean isVal(String s,int left, int right,int[][]pall){
-           --right;
-           int start = left;
-           int end = right;
-           if(pall[start][end] !=-1){
-            if(pall[start][end] == 0)
-            return false;
-            
-            return true;
-           }
-        while(left<right){
-            if(s.charAt(left) != s.charAt(right)){
-                pall[left][right] = 0;
-                pall[start][end] = 0;
-            return false;
-            }
+    public boolean isVal(String s, int left, int right, int[][] pal) {
 
-            left++;
-            right--;
+        if (left >= right) {
+            return true;
         }
-        pall[start][end] = 1;
-        return true;
+
+        if (pal[left][right] != -1) {
+            return pal[left][right] == 1;
+        }
+
+        if (s.charAt(left) == s.charAt(right) && isVal(s, left + 1, right - 1, pal)) {
+            pal[left][right] = 1;
+            return true;
+        }
+
+        pal[left][right] = 0;
+        return false;
     }
+
     public int minCut(String s) {
-        int n= s.length();
+        int n = s.length();
         int[][] pall = new int[n][n];
-        for(int[]a : pall){
-            Arrays.fill(a,-1);
+        for (int[] a : pall) {
+            Arrays.fill(a, -1);
         }
         int[] dp = new int[n];
-        
-        for(int a=0;a<dp.length;a++){
+
+        for (int a = 0; a < dp.length; a++) {
             dp[a] = -1;
         }
-        return helper(s,0,dp,pall);
+        return helper(s, 0, dp, pall);
     }
-    public int helper(String s, int index, int[]dp,int[][] pall){
-        
-        if(index == s.length()){
+
+    public int helper(String s, int index, int[] dp, int[][] pall) {
+
+        if (index == s.length()) {
             return -1;
         }
-        if(dp[index] != -1)
-        return dp[index];
+        if (dp[index] != -1)
+            return dp[index];
 
         int best = 2000;
-        for(int i= index; i<s.length(); i++){
-            if(isVal(s,index,i+1,pall)){
-               int res = 1+helper(s,i+1,dp,pall);
-               best = Math.min(best,res);
+        for (int i = index; i < s.length(); i++) {
+            if (isVal(s, index, i, pall)) {
+                int res = 1 + helper(s, i + 1, dp, pall);
+                best = Math.min(best, res);
             }
         }
         dp[index] = best;
