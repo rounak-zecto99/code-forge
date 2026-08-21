165c4fe55eb065efeec424fcad46dd4d1bf11bde
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 00:29:38 +0530

    Time: 14 ms (97.41%), Space: 47.6 MB (43.2%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,54 +1,20 @@
 class Solution {
-    public boolean isVal(String s, int left, int right, int[][] pal) {
-
-        if (left >= right) {
-             pal[left][right] = 1;
-            return true;
-        }
-
-        if (pal[left][right] != -1) {
-            return pal[left][right] == 1;
-        }
-
-        if (s.charAt(left) == s.charAt(right) && isVal(s, left + 1, right - 1, pal)) {
-            pal[left][right] = 1;
-            return true;
-        }
-
-        pal[left][right] = 0;
-        return false;
-    }
-
-    public int minCut(String s) {
-        int n = s.length();
-        int[][] pall = new int[n][n];
-        for (int[] a : pall) {
-            Arrays.fill(a, -1);
-        }
-        int[] dp = new int[n];
-
-        for (int a = 0; a < dp.length; a++) {
-            dp[a] = -1;
-        }
-        return helper(s, 0, dp, pall);
-    }
-
-    public int helper(String s, int index, int[] dp, int[][] pall) {
-
-        if (index == s.length()) {
-            return -1;
-        }
-        if (dp[index] != -1)
-            return dp[index];
-
-        int best = 2000;
-        for (int i = index; i < s.length(); i++) {
-            if (isVal(s, index, i, pall)) {
-                int res = 1 + helper(s, i + 1, dp, pall);
-                best = Math.min(best, res);
+   public int minCut(String s) {
+    char[] c = s.toCharArray();
+    int n = c.length;
+    int[] cut = new int[n];
+    boolean[][] pal = new boolean[n][n];
+    
+    for(int i = 0; i < n; i++) {
+        int min = i;
+        for(int j = 0; j <= i; j++) {
+            if(c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                pal[j][i] = true;  
+                min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
             }
         }
-        dp[index] = best;
-        return best;
+        cut[i] = min;
     }
+    return cut[n - 1];
+}
 }
