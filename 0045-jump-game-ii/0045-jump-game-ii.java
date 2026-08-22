35e082d8c5330113bcfb0242d7460c86bc6e4135
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 22:22:14 +0530

    Time: 1 ms (99.78%), Space: 47.4 MB (21.43%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -3,31 +3,19 @@
 // jumps → number of jumps taken
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int [] dp = new int[n];
-        for(int a=0; a<dp.length ;a++){
-            dp[a] = -1;
-        }
-        return helper(nums,0,dp);
+        int r =0;
+        int l =0;
+        int jumps =0;
+    
+    while(r<nums.length-1){
+    int farthest = 0;
+     for(int i=l;i<=r;i++){
+        farthest = Math.max(farthest,i+nums[i]);
+     }
+     l= r+1;
+     r = farthest;
+     jumps++;
     }
-    public int helper(int[] nums, int index, int []dp){
-        if(index >= nums.length-1){
-            return 0;
-        }
-        if(dp[index]!=-1)
-        return dp[index];
-
-        int best = Integer.MAX_VALUE;
-
-        for(int i= index+1;i<=index+nums[index];i++){
-       int result = helper(nums, i, dp);
-
-           if (result != Integer.MAX_VALUE) {
-           result = 1 + result;
-           best = Math.min(best, result);
-            }
-        }
-        dp[index]= best;
-        return best;
+    return jumps;
     }
 }
