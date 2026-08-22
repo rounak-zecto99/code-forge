26d07f2e8fda85354c74bfca5385f978d3fc53a2
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 13:31:30 +0530

    Time: 1 ms (98.77%), Space: 79.9 MB (96.66%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,35 +1,23 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        if(nums2.length < nums1.length)
-         return getCommon(nums2,nums1);
-        
-        int ans=-1;
+        int ans =-1;
+        int s=nums1.length;
+        int t=nums2.length;
+        int i=0;
+        int j=0;
 
-         for(int i=0; i<nums1.length; i++){
-         if(binary(nums2,nums1[i])){
-            ans = nums1[i];
-            break;
-         }
-         }
-         return ans;
-    }
-    public boolean binary(int []nums, int target){
-        int start =0;
-        int end = nums.length-1;
-        int mid ;
-        while(start<=end){
-        mid = start+(end-start)/2;
-
-        if(nums[mid] == target){
-            return true;
+        while(i<s && j<t){
+            if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                ans = nums1[i];
+                break;
+            }
         }
-        else if(nums[mid] < target){
-            start = mid+1;
-        }
-        else{
-            end = mid -1;
-        }
-        }
-        return false;
+        return ans;
     }
 }
