7ee1a7e9f6411f55f40c9ea7049ba640fe29a5c1
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 22:29:08 +0530

    Time: 1 ms (99.78%), Space: 47.2 MB (76.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -6,9 +6,9 @@ class Solution {
         int r =0;
         int l =0;
         int jumps =0;
-    
+            int farthest = 0;
     while(r<nums.length-1){
-    int farthest = 0;
+;
      for(int i=l;i<=r;i++){
         farthest = Math.max(farthest,i+nums[i]);
      }
