bccba391b4707c64b83cbd08b83125f025f7e417
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-22 18:40:51 +0530

    Time: 324 ms (6.68%), Space: 48 MB (43.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,26 @@
 class Solution {
     public boolean canJump(int[] nums) {
- 
-        Boolean[]dp = new Boolean[nums.length];
-               return helper(nums,0,dp);
+        Boolean[] dp = new Boolean[nums.length];
+        return helper(nums, 0, dp);
     }
-    public boolean helper(int[] nums, int index, Boolean[]dp){
-        if(index >= nums.length-1){
+
+    public boolean helper(int[] nums, int index, Boolean[] dp) {
+
+        if(index >= nums.length - 1) {
             return true;
         }
-        if(dp[index]!=null)
-        return dp[index];
 
-        for(int i=index+1; i<=index+nums[index]; i++){
-
-                if(helper(nums,i,dp)){
-                    dp[index] = true;
-                    return true;
-                }
-
+        if(dp[index] != null) {
+            return dp[index];
         }
+
+        for(int i = index + 1; i <= index + nums[index]; i++) {
+            if(helper(nums, i, dp)) {
+                dp[index] = true;
+                return true;
+            }
+        }
+
         dp[index] = false;
         return false;
     }
