c9e7ae9ff0ecf2f8e48f4c2fea8fc5cc572d6f55
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-28 21:04:29 +0530

    Time: 104 ms (5.02%), Space: 50.2 MB (57.29%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        int left = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+            heap.offer(new int[]{nums[i], i});
+
+            while (!heap.isEmpty() && (!map.containsKey(heap.peek()[0]) || heap.peek()[1] != map.get(heap.peek()[0]))) {
+                heap.poll();
+            }
+            int minKey = heap.peek()[0];
+            int boundary = heap.peek()[1];
+
+            if (map.size() > k) {
+
+                map.remove(minKey);
+                left = boundary + 1;
+                heap.poll();
+
+            }
+            while (!heap.isEmpty() && (!map.containsKey(heap.peek()[0]) || heap.peek()[1] != map.get(heap.peek()[0]))) {
+                heap.poll();
+            }
+            boundary = heap.peek()[1];
+
+            if (map.size() == k) {
+                count += boundary - left + 1;
+            }
+        }
+        return count;
+    }
+}
