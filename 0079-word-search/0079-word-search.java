a6d5a379eab9043c8b37899e1606e6f096dc90c9
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-29 14:27:24 +0530

    Time: 72 ms (97.09%), Space: 42.8 MB (88.09%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,54 +2,51 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        // boolean [][] taken = new boolean[m][n];
-        int pos=0;
-        char initial = word.charAt(0);
 
-        while(pos<m*n){
-            int row = pos/n;
-            int col = pos%n;
+        int totalpos = m * n;
+        for (int pos = 0; pos < totalpos; pos++) {
+            int row = pos / n;
+            int col = pos % n;
 
-            if(board[row][col] == initial){
-                if(helper(board,word,1,row,col)){
+            if (board[row][col] == word.charAt(0)) {
+                if (helper(board, word, 1, row, col, m, n)) {
                     return true;
                 }
             }
-            pos++;
         }
         return false;
     }
-    public boolean helper(char[][] board, String word, int index, int row, int col){
-        if(index == word.length()){
+
+    public boolean helper(char[][] board, String word, int index, int row, int col, int m, int n) {
+        if (index == word.length()) {
             return true;
         }
-        char currw = board[row][col];
+
+        char curr = board[row][col];
         board[row][col] = '.';
         char nextword = word.charAt(index);
- 
-        if(row>0 && board[row-1][col] == nextword){
-           if(helper(board,word,index+1,row-1,col)){
-            return true;
-           }
-      }
-         if(col>0 && board[row][col-1] == nextword){
-           if(helper(board,word,index+1,row,col-1)){
-            return true;
-           }
-        }
-         if(row<board.length-1 && board[row+1][col] == nextword){
-         if(helper(board,word,index+1,row+1,col)){
-            return true;
-           }
 
+        if (row > 0 && board[row - 1][col] == nextword) {
+            if (helper(board, word, index + 1, row - 1, col, m, n)) {
+                return true;
+            }
         }
-         if(col<board[0].length-1 && board[row][col+1] == nextword){
-            if(helper(board,word,index+1,row,col+1)){
-            return true;
-           }
-
+        if (col > 0 && board[row][col - 1] == nextword) {
+            if (helper(board, word, index + 1, row, col - 1, m, n)) {
+                return true;
+            }
         }
-         board[row][col] = currw;
+        if (row < m - 1 && board[row + 1][col] == nextword) {
+            if (helper(board, word, index + 1, row + 1, col, m, n)) {
+                return true;
+            }
+        }
+        if (col < n - 1 && board[row][col + 1] == nextword) {
+            if (helper(board, word, index + 1, row, col + 1, m, n)) {
+                return true;
+            }
+        }
+        board[row][col] = curr;
         return false;
     }
 }
