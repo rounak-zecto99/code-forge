9fc2ef69f446194439ce68d09419bbc09eb71e5d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-29 19:57:57 +0530

    Time: 46 ms (97.13%), Space: 43.1 MB (10.67%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,52 +1,53 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        int n = 9;
-        boolean[][] rows = new boolean[n][n + 1];
-        boolean[][] columns = new boolean[n][n + 1];
-        boolean[][][] grid = new boolean[3][3][n + 1];
+        int n=9;
+        boolean [][] rows = new boolean [n][n+1];
+        boolean [][] col = new boolean [n][n+1];
+        boolean [][][] grid = new boolean[3][3][n+1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n;j++){
                 int curr = board[i][j] - '0';
-                if (curr > 0 && curr < 10) {
+                if (curr > 0 && curr < 10){
                     rows[i][curr] = true;
-                    columns[j][curr] = true;
-                    grid[i / 3][j / 3][curr] = true;
+                    col[j][curr] = true;
+                    grid[i/3][j/3][curr] = true;
                 }
             }
-        }
-        helper(board, rows, columns, grid, 0);
-    }
-
-    public boolean helper(char[][] board, boolean[][] rows, boolean[][] columns, boolean[][][] grid, int pos) {
-        if (pos == 81) {
-            return true;
-        }
-        int row = pos / 9;
-        int col = pos % 9;
-
-        if (board[row][col] != '.')  return helper(board, rows, columns, grid, pos + 1);
-        for (int i = 1; i <= 9; i++) {
-            int curr = i;
-            if (rows[row][curr] || columns[col][curr] || grid[row / 3][col / 3][curr]) {
-                continue;
             }
-            rows[row][curr] = true;
-            columns[col][curr] = true;
-            grid[row / 3][col / 3][curr] = true;
-
-
-            board[row][col] = (char) (curr + '0');
-            if (helper(board, rows, columns, grid, pos + 1)) {
+            helper(board,rows,col,grid,0);
+        }
+        public boolean helper(char[][] board,boolean [][] rows,boolean [][] col,boolean [][][] grid,int pos){
+            if(pos == 81){
                 return true;
             }
-            board[row][col] = '.';
+            int row = pos/9;
+            int c = pos%9;
 
-            rows[row][curr] = false;
-            columns[col][curr] = false;
-            grid[row / 3][col / 3][curr] = false;
+            if(board[row][c]!='.') 
+            return helper(board,rows,col,grid,pos+1);
+
+            for(int curr=1; curr<=9; curr++){
+                if(rows[row][curr]||col[c][curr]||grid[row/3][c/3][curr]){
+                    continue;
+                }
+                rows[row][curr] = true;
+                col[c][curr] = true;
+                grid[row/3][c/3][curr] = true;
+
+                board[row][c] = (char)(curr +'0');
+
+                if(helper(board,rows,col,grid,pos+1)){
+                    return true;
+                }
+                
+                rows[row][curr] = false;
+                col[c][curr] = false;
+                grid[row/3][c/3][curr] = false;
+            }
+            board[row][c] = '.';
+              
+              return false;
+
         }
-        return false;
-    }
-}
-
+ }
