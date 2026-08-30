6b6600222f9aba4aefb50da66d251c0ac8c49902
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-08-31 01:49:29 +0530

    Time: 12 ms (98.68%), Space: 46.4 MB (64.45%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,112 +1,90 @@
-import java.util.*;
-
 class Solution {
 
-    public void pse(int[] nums, int[] pse) {
-        Deque<Integer> stack = new ArrayDeque<>();
+public void pse(int[] nums, int[] pse) {
+    int n = nums.length;
+    int[] stack = new int[n];
+    int top = -1;
 
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                pse[i] = -1;
-            } else {
-                pse[i] = stack.peek();
-            }
-
-            stack.push(i);
-        }
-    }
-
-    public void nse(int[] nums, int[] nse) {
-        Deque<Integer> stack = new ArrayDeque<>();
-
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                nse[i] = nums.length;
-            } else {
-                nse[i] = stack.peek();
-            }
-
-            stack.push(i);
-        }
-    }
-
-    public long subArrayRanges(int[] nums) {
-        int n = nums.length;
-
-        int[] pse = new int[n];
-        pse(nums, pse);
-
-        int[] nse = new int[n];
-        nse(nums, nse);
-
-        int[] pge = new int[n];
-        pge(nums, pge);
-
-        int[] nge = new int[n];
-        nge(nums, nge);
-
-        long sumMin = 0;
-        long countmin = 0;
-
-        long sumMax = 0;
-        long countmax = 0;
-
-        long finalsum = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            countmin = (long) (i - pse[i]) * (nse[i] - i);
-            sumMin += countmin * nums[i];
-
-            countmax = (long) (i - pge[i]) * (nge[i] - i);
-            sumMax += countmax * nums[i];
+    for (int i = 0; i < n; i++) {
+        while (top != -1 && nums[stack[top]] >= nums[i]) {
+            top--;
         }
 
-        finalsum = sumMax - sumMin;
-        return finalsum;
+        pse[i] = (top == -1) ? -1 : stack[top];
+        stack[++top] = i;
     }
+}
 
-    public void pge(int[] nums, int[] pge) {
-        Deque<Integer> stack = new ArrayDeque<>();
+public void nse(int[] nums, int[] nse) {
+    int n = nums.length;
+    int[] stack = new int[n];
+    int top = -1;
 
-        for (int i = 0; i < nums.length; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] <= nums[i]) {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                pge[i] = -1;
-            } else {
-                pge[i] = stack.peek();
-            }
-
-            stack.push(i);
+    for (int i = n - 1; i >= 0; i--) {
+        while (top != -1 && nums[stack[top]] > nums[i]) {
+            top--;
         }
+
+        nse[i] = (top == -1) ? n : stack[top];
+        stack[++top] = i;
     }
+}
 
-    public void nge(int[] nums, int[] nge) {
-        Deque<Integer> stack = new ArrayDeque<>();
+public void pge(int[] nums, int[] pge) {
+    int n = nums.length;
+    int[] stack = new int[n];
+    int top = -1;
 
-        for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-                stack.pop();
-            }
-
-            if (stack.isEmpty()) {
-                nge[i] = nums.length;
-            } else {
-                nge[i] = stack.peek();
-            }
-
-            stack.push(i);
+    for (int i = 0; i < n; i++) {
+        while (top != -1 && nums[stack[top]] <= nums[i]) {
+            top--;
         }
+
+        pge[i] = (top == -1) ? -1 : stack[top];
+        stack[++top] = i;
     }
+}
+
+public void nge(int[] nums, int[] nge) {
+    int n = nums.length;
+    int[] stack = new int[n];
+    int top = -1;
+
+    for (int i = n - 1; i >= 0; i--) {
+        while (top != -1 && nums[stack[top]] < nums[i]) {
+            top--;
+        }
+
+        nge[i] = (top == -1) ? n : stack[top];
+        stack[++top] = i;
+    }
+}
+
+public long subArrayRanges(int[] nums) {
+    int n = nums.length;
+
+    int[] pse = new int[n];
+    int[] nse = new int[n];
+    int[] pge = new int[n];
+    int[] nge = new int[n];
+
+    pse(nums, pse);
+    nse(nums, nse);
+    pge(nums, pge);
+    nge(nums, nge);
+
+    long sumMin = 0;
+    long sumMax = 0;
+
+    for (int i = 0; i < n; i++) {
+        long countMin = (long) (i - pse[i]) * (nse[i] - i);
+        sumMin += countMin * nums[i];
+
+        long countMax = (long) (i - pge[i]) * (nge[i] - i);
+        sumMax += countMax * nums[i];
+    }
+
+    return sumMax - sumMin;
+}
+
 }
