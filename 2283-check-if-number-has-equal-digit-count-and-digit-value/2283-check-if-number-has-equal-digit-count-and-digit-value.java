e3ebfe697f436925cabc14faca583fd80682b951
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-01 14:20:43 +0530

    Time: 0 ms (100%), Space: 42.8 MB (96.04%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -3,15 +3,11 @@ class Solution {
         int [] freq = new int[10];
         
         for(int i=0; i<num.length(); i++){
-            freq[i]+=num.charAt(i) - '0';
+           freq[num.charAt(i)-'0']++;
         }
-        for(int i=0; i<num.length(); i++){
-            freq[num.charAt(i) - '0']--;
-        }
-        for(int nums:freq){
-            if(nums!=0){
-                return false;
-            }
+        for(int i=0; i<num.length();i++){
+           if(freq[i] != (num.charAt(i)-'0'))
+           return false;
         }
         return true;
     }
