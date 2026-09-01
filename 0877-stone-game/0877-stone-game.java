d09ba4b0e60a1367cbdd6fe38b01a943199bd65e
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-02 01:49:48 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.83%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,31 +1,23 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
-
         if((n&1) == 0)
         return true;
-
-        Integer[][] dp = new Integer[n][n];
-
-        int res = helper(piles, 0, n - 1, dp);
-
-        if (res > 0)
+        int res = helper(piles,0,piles.length - 1);
+        
+        if(res>0)
             return true;
 
         return false;
 
     }
+    public int helper(int[]piles , int left , int right){
+        if(left == right)
+        return piles[left];
 
-    public int helper(int[] piles, int left, int right, Integer[][] dp) {
-        if (left == right)
-            return piles[left];
+        int choice1 = piles[left] - helper(piles,left+1,right);
+        int choice2 = piles[right] - helper(piles,left,right-1);
 
-        if (dp[left][right] != null)
-            return dp[left][right];
-
-        int choice1 = piles[left] - helper(piles, left + 1, right, dp);
-        int choice2 = piles[right] - helper(piles, left, right - 1, dp);
-
-        return dp[left][right] = Math.max(choice1, choice2);
+        return Math.max(choice1,choice2);
     }
 }
