12a23714dc004b8e44f9f5b4949a6cac9236b4b6
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-01 23:04:05 +0530

    Time: 0 ms (100%), Space: 42.8 MB (54.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        return helper(nums,0,nums.length-1) >=0;
+        Integer [][]dp = new Integer[nums.length][nums.length];
+        return helper(nums,0,nums.length-1,dp) >=0;
     }
-    public int helper(int[] nums,int left, int right){
+    public int helper(int[] nums,int left, int right,Integer[][]dp){
         if(left == right)
         return nums[left];
 
-        int leftM = nums[left] - helper(nums,left+1,right);
-        int rightM = nums[right] - helper(nums,left,right-1);
+        if(dp[left][right]!=null){
+            return dp[left][right];
+        }
 
-        return Math.max(leftM,rightM);
+        int leftM = nums[left] - helper(nums,left+1,right,dp);
+        int rightM = nums[right] - helper(nums,left,right-1,dp);
+
+        return dp[left][right] = Math.max(leftM,rightM);
     }
 }
