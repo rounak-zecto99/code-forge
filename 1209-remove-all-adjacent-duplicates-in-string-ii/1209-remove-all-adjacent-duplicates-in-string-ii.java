325ac31d28c7ac522977fbb41f74b02feb9a733c
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-01 22:47:55 +0530

    Time: 28 ms (61.72%), Space: 47.2 MB (69.96%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,40 +1,25 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack <int[]> stack = new Stack<>();
-        boolean dup = false;
+        Stack<int[]> stack = new Stack<>();
 
-        for(int i=0; i<s.length(); i++){
-           
-            if(stack.isEmpty()){
-                stack.push(new int[]{s.charAt(i),1});
+        for(char ch: s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek()[0] == ch){
+                stack.peek()[1]++;
+            }else{
+                stack.push(new int[]{ch,1});
             }
-            else{
-                if(stack.peek()[0] == s.charAt(i)){
-                    stack.push(new int[]{s.charAt(i),stack.peek()[1]+1});
-                    if(stack.peek()[1] == k){
-                        dup = true;
-                        for(int j=k;j>0;j--){
-                            stack.pop();
-                        }
-                    }
-                }
-                else{
-                    stack.push(new int[]{s.charAt(i),1});
-                }
+            if(stack.peek()[1] == k){
+                stack.pop();
             }
         }
-        if(!dup)
-        return s;
-
         StringBuilder sb = new StringBuilder();
-
-        while(!stack.isEmpty()){
-            char curr =(char) stack.peek()[0];
-            sb.append(curr);
-        //    System.out.println(curr);
-           stack.pop();
+        for(int []ar : stack){
+            for(int i=0; i<ar[1]; i++){
+               sb.append((char)ar[0]);
+            }
         }
-        // return s;
-        return sb.reverse().toString();
+        
+        return sb.toString();
+
     }
 }
