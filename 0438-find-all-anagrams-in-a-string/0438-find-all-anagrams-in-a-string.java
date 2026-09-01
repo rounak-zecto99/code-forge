97ece3edc8459871ca6a2b3aca9285abc44d9958
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-02 01:31:46 +0530

    Time: 11 ms (66.9%), Space: 46.8 MB (58.17%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List <Integer> list = new ArrayList<>();
-        int []need = new int [26];
-        
-        for(int n: p.toCharArray()){
+        List<Integer> list = new ArrayList<>();
+        int[] need = new int[26];
+
+        for (int n : p.toCharArray()) {
             need[n - 'a']++;
         }
         int left = 0;
         int req = p.length();
 
-        for(int i=0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             int a = s.charAt(i);
 
-            if(need[a-'a']>0)
-            req--;
+            if (need[a - 'a'] > 0)
+                req--;
 
-            need[a-'a']--;
+            need[a - 'a']--;
 
-            if(i-left+1>p.length()){
+            if (i - left + 1 > p.length()) {
                 int last = s.charAt(left++);
-                 need[last-'a']++;
+                need[last - 'a']++;
 
-                 if(need[last-'a']>0)
+                if (need[last - 'a'] > 0)
                     req++;
             }
-            if(req == 0){
+            if (req == 0) {
                 list.add(left);
             }
-            }         
+        }
 
         return list;
     }
