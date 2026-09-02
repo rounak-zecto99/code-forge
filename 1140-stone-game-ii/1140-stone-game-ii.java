4f91397a018400a3f4ab6b48e4395a3de33ca5aa
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-02 15:26:57 +0530

    Time: 2 ms (100%), Space: 44.5 MB (36.87%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,32 +1,46 @@
-// What information changes after every move?
-// What choices does a player have?
-// What information is needed to describe the next turn?
 class Solution {
-    public int stoneGameII(int[] piles) {
-        int len = piles.length;
-        Integer [][] dp = new Integer[len][len];
+private int[][] dp;
 
-        int [] suffix = new int[len+1];
+public int stoneGameII(int[] piles) {
+    int n = piles.length;
 
-        for(int i = len -1; i>=0; i--){
-            suffix[i] = piles[i] + suffix[i+1];
-        }
-        return helper(piles,0,1,suffix,dp);
+    // suffix[i] = total stones from i to n - 1
+    int[] suffix = new int[n + 1];
 
+    for (int i = n - 1; i >= 0; i--) {
+        suffix[i] = piles[i] + suffix[i + 1];
     }
-    public int helper(int[] piles,int curr,int M,int[]suffix,Integer [][] dp){
-         if(curr+2*M >= piles.length)
-         return suffix[curr];
 
-         if(dp[curr][M]!=null)
-         return dp[curr][M];
-         
-         int best = Integer.MIN_VALUE;
-         for(int X=1 ;X<=2*M; X++){
-            int opponent = helper(piles,curr+X,Math.max(X,M),suffix,dp);
-            int myscore = suffix[curr] -  opponent;
-            best = Math.max(best,myscore);
-         }
-         return dp[curr][M] = best;
+    dp = new int[n][n + 1];
+
+    return helper(0, 1, piles, suffix);
+}
+
+private int helper(int curr, int M, int[] piles, int[] suffix) {
+
+    // Current player can take everything remaining
+    if (curr + 2 * M >= piles.length) {
+        return suffix[curr];
     }
+
+    if (dp[curr][M] != 0) {
+        return dp[curr][M];
+    }
+
+    int maxScore = 0;
+
+    // X = number of piles taken
+    for (int X = 1; X <= 2 * M; X++) {
+
+        int opponentScore =
+            helper(curr + X, Math.max(M, X), piles, suffix);
+
+        int myScore = suffix[curr] - opponentScore;
+
+        maxScore = Math.max(maxScore, myScore);
+    }
+
+    return dp[curr][M] = maxScore;
+}
+
 }
