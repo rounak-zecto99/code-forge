74ccbcd4f014f61919196034b45418d8496dce1d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-04 00:55:04 +0530

    Time: 23 ms (92.81%), Space: 81.4 MB (14.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,57 +1,18 @@
 class Solution {
-    int[] stack;
-  public void pse(int[] heights, int[] pse) {
-    int n = pse.length;
-    stack = new int[n];
-    int top = -1;
-
-    for (int i = 0; i < n; i++) {
-        // Pop while elements on stack are > current element
-        while (top >= 0 && heights[stack[top]] > heights[i]) {
-            top--;
-        }
-
-        // If stack is empty, PSE is -1; otherwise stack[top]
-        pse[i] = (top == -1) ? -1 : stack[top];
-
-        // Push current index onto the stack
-        stack[++top] = i;
-    }
-}
-
-public void nse(int[] heights, int[] nse) {
-    int n = nse.length;
-     stack = new int[n];
-    int top = -1; // -1 represents an empty stack
-
-    for (int i = n - 1; i >= 0; i--) {
-        // Pop while elements on stack are >= current element
-        while (top >= 0 && heights[stack[top]] >= heights[i]) {
-            top--;
-        }
-
-        // If stack is empty, NSE is heights.length; otherwise stack[top]
-        nse[i] = (top == -1) ? n : stack[top];
-
-        // Push current index onto the stack
-        stack[++top] = i;
-    }
-}
-
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] pse = new int[n];
-        pse(heights, pse);
-        int[] nse = new int[n];
-        nse(heights, nse);
-
+        Deque<Integer> stack = new ArrayDeque<>();
         int maxarea = 0;
 
-        for (int i = 0; i < heights.length; i++) {
-            int previous = i - pse[i] - 1;
-            int forward = nse[i] - i;
-            int area = heights[i] * (previous + forward);
-            maxarea = Math.max(maxarea, area);
+        for (int i = 0; i <= heights.length; i++) {
+            int currHeight = (i == heights.length) ? 0 : heights[i];
+          while(!stack.isEmpty() && heights[stack.peek()]>currHeight){
+            int a = stack.pop();
+            int backlimit = stack.isEmpty()?-1:stack.peek();
+            int area = heights[a]*(i-backlimit-1);
+            maxarea = Math.max(maxarea,area);
+          }
+          stack.push(i);
         }
         return maxarea;
     }
