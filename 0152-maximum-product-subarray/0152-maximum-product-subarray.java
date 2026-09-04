31ff99f04ed3675ddf946175516b8143235dc5cc
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-04 18:49:41 +0530

    Time: 2 ms (72.08%), Space: 48.6 MB (15.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,23 @@
 class Solution {
+    int best = Integer.MIN_VALUE;
     public int maxProduct(int[] nums) {
-        int[]best = {Integer.MIN_VALUE};
-       helper(nums,0,best);
-       return best[0];
+       
+       helper(nums,0);
+       return best;
     }
-    public int[] helper(int[] nums, int index,int[]best){
+    public int[] helper(int[] nums, int index){
         if (index == nums.length - 1) {
-            best[0] = nums[index];
+            best = Math.max(best, nums[index]);
        return new int[]{nums[index], nums[index]};
      }
-     int [] arr = helper(nums,index+1,best);
+     int [] arr = helper(nums,index+1);
      int max = nums[index]*arr[0];
      int min = nums[index]*arr[1];
 
      int amax = Math.max(max,Math.max(nums[index],min));
      int amin = Math.min(max,Math.min(nums[index],min));
      
-     best[0] = Math.max(best[0],amax);
+     best = Math.max(best,amax);
      return new int[]{amax,amin};
 
     }
