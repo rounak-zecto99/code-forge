2308a075ee2a8ab432211a89d5277259e181edf8
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-04 12:22:39 +0530

    Time: 18 ms (39.91%), Space: 45.8 MB (14.85%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -3,6 +3,9 @@ class Solution {
         Arrays.sort(nums);
         int currsum = nums[0]+nums[1]+nums[2];
         for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
             int start = i+1;
             int end = nums.length-1;
 
