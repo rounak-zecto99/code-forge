dc813fdd5395cf0529c9012054dec0193795403c
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-04 20:29:47 +0530

    Time: 4 ms (8.74%), Space: 65.4 MB (66.67%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int freq = 0;
+        int left =0;
+        int size =0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1)
+            freq ++ ;
+
+            if((i-left+1)-freq>1){
+                if(nums[left++] == 1)
+                freq--;
+            }
+            size = Math.max(size,i-left+1);
+        }
+        return size-1;
+    }
+}
