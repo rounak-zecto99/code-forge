45152d85e88b1583d743e71e33c06bef39259e87
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-04 12:21:26 +0530

    Time: 19 ms (19.88%), Space: 44.9 MB (96.79%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,32 +1,28 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int currentSum = nums[0]+nums[1]+nums[2];
+        int currsum = nums[0]+nums[1]+nums[2];
         for(int i=0; i<nums.length-2; i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                continue;
-            }
             int start = i+1;
-            int end = nums.length -1;
-             
-             while(start<end){
-                int newsum = nums[i] + nums[start] + nums[end];
+            int end = nums.length-1;
 
-                if(Math.abs(target - newsum) < Math.abs(target - currentSum)){
-                    currentSum = newsum;
+            while(start<end){
+                int newsum = nums[i]+nums[start]+nums[end];
+
+                if(Math.abs(target-newsum)<Math.abs(target-currsum)){
+                    currsum = newsum;
                 }
-                if(target>newsum){
-                    start ++;
+                if(newsum<target){
+                    start++;
                 }
-                else if(target<newsum){
+                else if(newsum>target){
                     end --;
                 }
                 else{
-                    currentSum = newsum;
                     return newsum;
                 }
-             }
+            }
         }
-        return currentSum;
+        return currsum;
     }
 }
