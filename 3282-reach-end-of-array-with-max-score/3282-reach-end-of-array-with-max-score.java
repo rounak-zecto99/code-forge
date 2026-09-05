60fc22ad3e19b40363d6668a92741809d285b009
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-06 02:20:30 +0530

    Time: 13 ms (8.85%), Space: 162.6 MB (54.87%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.java
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.java
@@ -1,46 +1,15 @@
 class Solution {
     public long findMaximumScore(List<Integer> nums) {
+     long score = 0;
+     int i = 0;
 
-        int n = nums.size();
-
-        // Pass 1: Find Next Greater Element
-        int[] nge = new int[n];
-        Arrays.fill(nge, -1);
-
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!stack.isEmpty()
-                    && nums.get(stack.peek()) <= nums.get(i)) {
-                stack.pop();
-            }
-
-            if (!stack.isEmpty()) {
-                nge[i] = stack.peek();
-            }
-
-            stack.push(i);
+     for(int j=1; j<nums.size() ; j++){
+        if(nums.get(j)>nums.get(i)){
+            score +=(long)(j-i) * nums.get(i);
+            i = j;
         }
-
-        // Pass 2: Follow the NGE jumps and calculate score
-        long score = 0;
-        int i = 0;
-
-        while (i < n - 1) {
-
-            int next = nge[i];
-
-            // No greater element → jump directly to end
-            if (next == -1) {
-                next = n - 1;
-            }
-
-            score += (long) (next - i) * nums.get(i);
-
-            i = next;
-        }
-
-        return score;
+     }
+     score += (long)(nums.size()-1-i)*nums.get(i);
+     return score;
     }
 }
