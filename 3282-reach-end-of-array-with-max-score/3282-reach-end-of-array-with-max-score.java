3887cb43cef071e6c3a2d0d3ead50c95028d6ba8
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-06 02:09:40 +0530

    Time: 12 ms (8.85%), Space: 164.6 MB (11.5%) - LeetHub

---
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long findMaximumScore(List<Integer> nums) {
+     long score = 0;
+     int i = 0;
+
+     for(int j=1; j<nums.size() ; j++){
+        if(nums.get(j)>nums.get(i)){
+            score +=(long)(j-i) * nums.get(i);
+            i = j;
+        }
+     }
+     score += (long)(nums.size()-1-i)*nums.get(i);
+     return score;
+    }
+}
