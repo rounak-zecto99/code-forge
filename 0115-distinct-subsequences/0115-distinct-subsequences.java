bbb0dc2b8c4661b55e44c1feed346afca1611112
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-07 00:17:24 +0530

    Time: 22 ms (24.24%), Space: 54.6 MB (27.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,19 +1,41 @@
 class Solution {
+
     public int numDistinct(String s, String t) {
 
-        int[] dp = new int[t.length() + 1];
-        dp[0] = 1;
+        if (s.length() < t.length())
+            return 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
 
-            for (int j = t.length() - 1; j >= 0; j--) {
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
 
-                if (s.charAt(i) == t.charAt(j)) {
-                    dp[j + 1] += dp[j];
-                }
-            }
+        return helper(s, t, 0, 0, dp);
+    }
+
+    public int helper(String s, String t, int index, int taken, int[][] dp) {
+
+        if (taken == t.length())
+            return 1;
+
+        if (index == s.length())
+            return 0;
+
+        if (dp[index][taken] != -1)
+            return dp[index][taken];
+
+        int ways = 0;
+
+        if (s.charAt(index) == t.charAt(taken)) {
+            // Take s[index] OR skip it
+            ways += helper(s, t, index + 1, taken + 1, dp);
+            ways += helper(s, t, index + 1, taken, dp);
+        } else {
+            // Cannot take s[index], so skip it
+            ways += helper(s, t, index + 1, taken, dp);
         }
 
-        return dp[t.length()];
+        return dp[index][taken] = ways;
     }
 }
+
