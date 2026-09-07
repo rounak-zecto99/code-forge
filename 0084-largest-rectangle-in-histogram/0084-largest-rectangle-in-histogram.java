bf39de8cbbb1ca33267f91272c1f9c7096ccec1d
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-07 21:11:04 +0530

    Time: 25 ms (88.7%), Space: 81.3 MB (17.6%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,28 +1,19 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
-        int[] stack = new int[n];
-        int top = -1;
-
+        Deque<Integer> stack = new ArrayDeque<>();
         int maxarea = 0;
 
         for (int i = 0; i <= n; i++) {
-            int currHeight = (i == n) ? 0 : heights[i];
-
-            while (top != -1 && heights[stack[top]] > currHeight) {
-                int index = stack[top--];
-
-                int left = (top == -1) ? -1 : stack[top];
-
-                int area = heights[index] * (i - left - 1);
-                maxarea = Math.max(maxarea, area);
-            }
-
-            if (i < n) {
-                stack[++top] = i;
-            }
+            int curr = i == n?0:heights[i];
+          while(!stack.isEmpty() && heights[stack.peek()]>curr){
+            int a = stack.pop();
+            int backlimit = stack.isEmpty()?-1:stack.peek();
+            int area = heights[a]*(i-backlimit-1);
+            maxarea = Math.max(maxarea,area);
+          }
+          stack.push(i);
         }
-
         return maxarea;
     }
 }
