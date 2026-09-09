b768a69396e9cadc19fe5d4831f128049fbbdc00
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-09 12:22:47 +0530

    Time: 1 ms (99.36%), Space: 42.5 MB (71.15%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,15 +1,11 @@
 class Solution {
     public long countCommas(long n) {
-        if (n <= 999) return 0;
+        long ans = 0;
 
-        long totalCommas = 0;
-        long start = 1000;
-
-        while (start <= n) {
-            totalCommas += n - start + 1;
-            start *= 1000;
+        for (long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
         }
 
-        return totalCommas;
+        return ans;
     }
 }
