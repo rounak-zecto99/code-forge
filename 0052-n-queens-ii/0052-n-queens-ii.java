122bf9723305b621550e323c56f4ca33594fac6f
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-11 23:43:07 +0530

    Time: 1 ms (94.97%), Space: 41.7 MB (96.58%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int totalNQueens(int n) {
+        int [] queens = new int[n];
+        boolean[] column = new boolean[n];
+        boolean[] diag1 = new boolean[2*n-1];
+        boolean[] diag2 = new boolean[2*n-1];
+
+        return helper(column,diag1,diag2,queens,0);
+    }
+    public int helper(boolean[] column,boolean[] diag1,boolean[] diag2,int []queens,int row){
+        if(row == queens.length){
+            return 1;
+        }
+        int ways = 0;
+
+        for(int col=0; col<queens.length; col++){
+            if(column[col]||diag1[row + col]||diag2[row - col + queens.length -1])
+            continue;
+            column[col] = true;
+            diag1[row + col] = true;
+            diag2[row - col + queens.length -1] = true;
+
+            queens[row] = col;
+            ways+= helper(column,diag1,diag2,queens,row+1);
+
+            column[col] = false;
+            diag1[row + col] = false;
+            diag2[row - col + queens.length -1] = false;
+        }
+        return ways;
+    }
+}
