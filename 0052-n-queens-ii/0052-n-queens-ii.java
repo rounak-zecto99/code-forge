530f76ba98e5ca99030963aa3d2a842bbc365cff
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-11 23:45:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.74%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,32 +1,28 @@
 class Solution {
     public int totalNQueens(int n) {
-        int [] queens = new int[n];
-        boolean[] column = new boolean[n];
-        boolean[] diag1 = new boolean[2*n-1];
-        boolean[] diag2 = new boolean[2*n-1];
-
-        return helper(column,diag1,diag2,queens,0);
+        return helper(n, 0, 0, 0);
     }
-    public int helper(boolean[] column,boolean[] diag1,boolean[] diag2,int []queens,int row){
-        if(row == queens.length){
+
+    private int helper(int n, int cols, int diag1, int diag2) {
+        if (cols == (1 << n) - 1) {
             return 1;
         }
+
+        int available = ((1 << n) - 1) & ~(cols | diag1 | diag2);
         int ways = 0;
 
-        for(int col=0; col<queens.length; col++){
-            if(column[col]||diag1[row + col]||diag2[row - col + queens.length -1])
-            continue;
-            column[col] = true;
-            diag1[row + col] = true;
-            diag2[row - col + queens.length -1] = true;
+        while (available != 0) {
+            int bit = available & -available;
+            available -= bit;
 
-            queens[row] = col;
-            ways+= helper(column,diag1,diag2,queens,row+1);
-
-            column[col] = false;
-            diag1[row + col] = false;
-            diag2[row - col + queens.length -1] = false;
+            ways += helper(
+                n,
+                cols | bit,
+                (diag1 | bit) << 1,
+                (diag2 | bit) >> 1
+            );
         }
+
         return ways;
     }
 }
