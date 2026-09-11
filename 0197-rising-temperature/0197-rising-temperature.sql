7d8f34446f07ead8456255ca69c39b1c2c819581
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-11 22:29:38 +0530

    Time: 467 ms (78.98%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
 SELECT a.id
-FROM Weather a 
-JOIN  Weather b
-ON TIMESTAMPDIFF(DAY, b.recordDate, a.recordDate) = 1
-WHERE a.temperature > b.temperature 
+FROM Weather a
+JOIN Weather b
+    ON TIMESTAMPDIFF(DAY, b.recordDate, a.recordDate) = 1
+WHERE a.temperature > b.temperature;
