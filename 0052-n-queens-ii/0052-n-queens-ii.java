496a3556b2ce8a5e6c88360845e05a09bb6f8102
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-11 23:58:27 +0530

    Time: 1 ms (94.97%), Space: 42 MB (57.05%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -3,31 +3,32 @@ class Solution {
         // int [] queens = new int[n];
         int queens = 0;
         boolean[] column = new boolean[n];
-        boolean[] diag1 = new boolean[2*n-1];
-        boolean[] diag2 = new boolean[2*n-1];
+        boolean[] diag1 = new boolean[2 * n - 1];
+        boolean[] diag2 = new boolean[2 * n - 1];
 
-        return helper(column,diag1,diag2,queens,0);
+        return helper(column, diag1, diag2, queens, 0);
     }
-    public int helper(boolean[] column,boolean[] diag1,boolean[] diag2,int queens,int row){
-        if(row == column.length){
+
+    public int helper(boolean[] column, boolean[] diag1, boolean[] diag2, int queens, int row) {
+        if (row == column.length) {
             return 1;
         }
         int ways = 0;
 
-        for(int col=0; col<column.length; col++){
-            if(column[col]||diag1[row + col]||diag2[row - col + column.length -1])
-            continue;
+        for (int col = 0; col < column.length; col++) {
+            if (column[col] || diag1[row + col] || diag2[row - col + column.length - 1])
+                continue;
             column[col] = true;
             diag1[row + col] = true;
-            diag2[row - col + column.length -1] = true;
+            diag2[row - col + column.length - 1] = true;
 
             queens++;
-            ways+= helper(column,diag1,diag2,queens,row+1);
-            
+            ways += helper(column, diag1, diag2, queens, row + 1);
+
             queens--;
             column[col] = false;
             diag1[row + col] = false;
-            diag2[row - col + column.length -1] = false;
+            diag2[row - col + column.length - 1] = false;
         }
         return ways;
     }
