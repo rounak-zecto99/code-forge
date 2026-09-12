26accccf6e9e9dab3b2f6ae746a32a0ef1dfa4ef
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-12 11:08:56 +0530

    Time: 14 ms (70.03%), Space: 47.7 MB (33.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,25 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int num : nums)
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-        List<Integer>[] bucket = new List[nums.length + 1];
-
-        for (var e : map.entrySet()) {
-            int freq = e.getValue();
-
-            if (bucket[freq] == null)
-                bucket[freq] = new ArrayList<>();
-
-            bucket[freq].add(e.getKey());
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
         }
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) ->
+        Integer.compare(a[1],b[1]));
 
-        int[] ans = new int[k];
-        int j = 0;
+        for(var a : map.entrySet()){
+            heap.offer(new int[]{a.getKey(),a.getValue()});
 
-        for (int freq = nums.length; freq >= 1 && j < k; freq--) {
-            if (bucket[freq] != null) {
-                for (int num : bucket[freq]) {
-                    ans[j++] = num;
-
-                    if (j == k)
-                        break;
-                }
-            }
+            if(heap.size()>k){
+                heap.poll();
+            }           
         }
-
+        int [] ans = new int[k];
+        int j = k-1;
+        for(int i=j;i>=0;i--){
+            ans[i] = heap.peek()[0];
+            heap.poll();
+        }
         return ans;
     }
 }
