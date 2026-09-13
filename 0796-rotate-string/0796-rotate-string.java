e8f7bbc26b93b95b980667de925992dde0101e4c
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-13 23:47:35 +0530

    Time: 2 ms (63.58%), Space: 43.4 MB (49.67%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,9 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length())
-        return false;
-
-        String a = s+s;
-        return a.contains(goal);
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
     }
 }
