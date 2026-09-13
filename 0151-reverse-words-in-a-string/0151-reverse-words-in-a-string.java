749a5ed80a57957c75c8e45f73a28d6c9be4e749
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-14 02:03:35 +0530

    Time: 4 ms (93.35%), Space: 43.6 MB (98.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,19 +1,59 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
+        char[] arr = s.toCharArray();
 
-        String[] words = s.split("\\s+");
+        // 1. Remove extra spaces
+        int n = cleanSpaces(arr);
 
-        StringBuilder ans = new StringBuilder();
+        // 2. Reverse entire string
+        reverse(arr, 0, n - 1);
 
-        for (int i = words.length - 1; i >= 0; i--) {
-            ans.append(words[i]);
+        // 3. Reverse each word
+        int start = 0;
 
-            if (i != 0) {
-                ans.append(" ");
+        for (int end = 0; end <= n; end++) {
+            if (end == n || arr[end] == ' ') {
+                reverse(arr, start, end - 1);
+                start = end + 1;
             }
         }
 
-        return ans.toString();
+        return new String(arr, 0, n);
+    }
+
+    private int cleanSpaces(char[] arr) {
+        int left = 0;
+        int right = 0;
+
+        while (right < arr.length) {
+            while (right < arr.length && arr[right] == ' ') {
+                right++;
+            }
+
+            while (right < arr.length && arr[right] != ' ') {
+                arr[left++] = arr[right++];
+            }
+
+            while (right < arr.length && arr[right] == ' ') {
+                right++;
+            }
+
+            if (right < arr.length) {
+                arr[left++] = ' ';
+            }
+        }
+
+        return left;
+    }
+
+    private void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
     }
 }
