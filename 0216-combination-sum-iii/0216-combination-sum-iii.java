990b08e7b809d3423dd0399d39bf62862d425f71
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-15 13:35:51 +0530

    Time: 0 ms (100%), Space: 42.6 MB (12.78%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -4,18 +4,19 @@ class Solution {
         helper(list,new ArrayList<>(),k,n,1);
         return list;
     }
-    public void helper(List<List<Integer>> list,List <Integer> row, int k, int n,int index){
-         if(row.size() == k && 0 == n ){
+    public void helper( List<List<Integer>> list , List <Integer> row , int k, int n,int index){
+
+        if(k == row.size() && n == 0){
             list.add(new ArrayList<>(row));
             return;
-         }
-         if(row.size() > k || n < 0 ) return;
+        }
+        if(k < row.size() || n < 0)
+        return;
 
-        for(int i= index ; i<=9 ; i++){
+        for(int i=index; i<=9; i++){
             row.add(i);
             helper(list,row,k,n-i,i+1);
             row.remove(row.size()-1);
         }
-
     }
 }
