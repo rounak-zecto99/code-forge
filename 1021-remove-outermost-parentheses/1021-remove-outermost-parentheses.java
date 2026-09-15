402f8ff87f5f436454519eda4e4adbfe75cf6bd3
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-15 17:38:18 +0530

    Time: 2 ms (99.79%), Space: 43.7 MB (27.54%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(') {
+                depth++;
+
+                if (depth > 1) {
+                    sb.append(ch);
+                }
+
+            } else {
+                if (depth > 1) {
+                    sb.append(ch);
+                }
+
+                depth--;
+            }
+        }
+
+        return sb.toString();
+    }
+}
