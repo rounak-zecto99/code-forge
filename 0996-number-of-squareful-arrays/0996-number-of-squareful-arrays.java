3d51c8244e836fd6a5af28589d80545753600b00
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-18 01:16:12 +0530

    Time: 2 ms (83.88%), Space: 42.9 MB (49.25%) - LeetHub

--- 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.java
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.java
@@ -1,51 +1,38 @@
 class Solution {
+    public boolean perfectsq(int num1, int num2){
+        int y = num1+num2;
+        int root = (int)Math.sqrt(y);
+        return root*root == y;
+    }
     public int numSquarefulPerms(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-
-        boolean[][] adj = new boolean[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int sum = nums[i] + nums[j];
-                int root = (int) Math.sqrt(sum);
-
-                if (root * root == sum) {
-                    adj[i][j] = true;
-                    adj[j][i] = true;
-                }
-            }
-        }
-
-        return backtrack(nums, adj, new boolean[n], 0, -1);
+        boolean []used = new boolean[nums.length];
+        return helper(0,0,nums,used);
     }
 
-    private int backtrack(int[] nums, boolean[][] adj,
-                          boolean[] used, int count, int prev) {
-
-        if (count == nums.length)
+    public int helper(int lastTaken,int taken,int[] nums, boolean []used){
+        if(taken == nums.length){
             return 1;
+        }
 
         int ways = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i=0; i<nums.length; i++){
+            if(used[i])
+            continue;
 
-            if (used[i])
-                continue;
+            if(i>0 && nums[i] == nums[i-1] && !used[i-1])
+            continue;
 
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
-                continue;
-
-            if (prev != -1 && !adj[prev][i])
-                continue;
+            if(taken>0 && !perfectsq(lastTaken,nums[i]))
+            continue;
 
             used[i] = true;
 
-            ways += backtrack(nums, adj, used, count + 1, i);
+           ways+= helper(nums[i],taken+1,nums,used);
 
             used[i] = false;
         }
-
         return ways;
     }
 }
