a54f86296220ad4e156cba4ee4c8409088318e49
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-18 11:29:10 +0530

    Time: 20 ms (30.62%), Space: 82.6 MB (47.73%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,36 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-
-        if ((long) m * k > bloomDay.length)
+        if ((long) m * k > (long) bloomDay.length)
             return -1;
 
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
+        int start = Integer.MAX_VALUE;
+        int end = 0;
 
-        for (int day : bloomDay) {
-            low = Math.min(low, day);
-            high = Math.max(high, day);
+        for (int x : bloomDay) {
+            start = Math.min(start, x);
+            end = Math.max(end, x);
         }
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
+        while (start < end) {
+            int days = start + ((end - start) >> 1);
+            int bouq = 0;
+            int left = 0;
 
-            if (canMake(bloomDay, m, k, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
-    }
-
-    private boolean canMake(int[] bloomDay, int m, int k, int days) {
-        int bouquets = 0;
-        int flowers = 0;
-
-        for (int bloom : bloomDay) {
-
-            if (bloom <= days) {
-                flowers++;
-
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
+            for (int i = 0; i < bloomDay.length; i++) {
+                if (days < bloomDay[i]) {
+                    left = i + 1;
                 }
-
-                if (bouquets == m)
-                    return true;
-
+                if (i - left + 1 == k) {
+                    bouq++;
+                    left = i + 1;
+                }
+            }
+            if (bouq < m) {
+                start = days + 1;
             } else {
-                flowers = 0;
+                end = days;
             }
         }
-
-        return false;
+        return end;
     }
 }
