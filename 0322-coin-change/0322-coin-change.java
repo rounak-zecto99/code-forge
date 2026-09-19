4c2bc462df6757efa3528232543923f1846622e7
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-19 19:48:02 +0530

    Time: 15 ms (83.82%), Space: 47.2 MB (17.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,44 +1,60 @@
 class Solution {
-
+    static int INF = 1_000_000_000;
+    
     public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int [][]dp = new int[n][amount+1];
+        // int ans = helper(coins,amount,n-1);
 
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,-2);
-
-        if (amount == 0)
-            return 0;
-
-        int ans = helper(coins, amount,dp);
-
-        if (ans == Integer.MAX_VALUE)
-            return -1;
-
-        return ans;
-    }
-
-    public int helper(int[] coins, int amount,int []dp) {
-
-        if (amount == 0)
-            return 0;
-
-        if (amount < 0)
-            return Integer.MAX_VALUE;
-
-        if(dp[amount]!=-2)
-        return dp[amount];
-
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < coins.length; i++) {
-
-            int res = helper(coins, amount - coins[i],dp);
-
-            if (res != Integer.MAX_VALUE) {
-                res = res + 1;
-                min = Math.min(res, min);
+        // if (ans >= INF) {
+        //  return -1;
+        // }
+        for(int T=0; T<=amount; T++){
+            if(T%coins[0] == 0){
+                dp[0][T] = T/coins[0];
+            }
+            else{
+                dp[0][T] = INF;
             }
         }
 
-        return dp[amount] = min;
+        for(int ind=1; ind<n; ind++){
+            for(int T=0; T<=amount; T++){
+                int notTake =dp[ind-1][T];
+                int take = INF;
+
+                if(coins[ind]<=T)
+                take = 1 + dp[ind][T-coins[ind]];
+
+                dp[ind][T] = Math.min(take,notTake);
+            }
+        }
+        int ans = dp[n-1][amount];
+
+        if(ans>=INF){
+            return -1;
+        }
+
+        return ans;
+
     }
+    // public int helper(int[]coins,int amount,int index){
+
+    //     if(index == 0){
+    //         if(amount%coins[0] == 0){
+    //             return amount/coins[0];
+    //         }
+    //         else{
+    //             return 1000_000_000;
+    //         }
+    //     }
+
+    //     int notTake =helper(coins,amount,index-1);
+    //     int take = Integer.MAX_VALUE;
+
+    //     if(coins[index]<=amount)
+    //     take = 1 + helper(coins,amount-coins[index],index);
+
+    //     return Math.min(take,notTake);
+    // }
 }
