fabf06dbba0bc53bc8361990ebfc6fd3607cf8b8
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-20 01:30:35 +0530

    Time: 0 ms (100%), Space: 42.6 MB (67.19%) - LeetHub

--- 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
@@ -1,56 +1,77 @@
 class Solution {
-    public String strWithout3a3b(int a, int b) {
+    // public String strWithout3a3b(int a, int b) {
 
-        StringBuilder sb = new StringBuilder();
-        boolean ap = false;
-        boolean bp = false;
+    //     StringBuilder sb = new StringBuilder();
+    //     boolean ap = false;
+    //     boolean bp = false;
 
-        while (a != 0 && b != 0) {
-            if (a - b >= 2 && !ap) {
-                sb.append("aa");
-                a-=2;
-                ap=true;
-                bp=false;
-            }
-            else if(b - a >= 2 && !bp){
-                sb.append("bb");
-                b-=2;
-                bp=true;
-                ap=false;
-            }
-            else if(a>b && !ap){
-                sb.append('a');
-                a--;
-                bp=true;
-                ap=false;
-            }
-            else if(b>a && !bp){
-                sb.append('b');
-                b--;
-                bp=true;
-                ap=false;
-            }
-            else if(!ap){
-                sb.append('a'); 
-                a--;
-                ap=true;
-                bp=false;
-            }
-            else{
-                sb.append('b');
-                b--;
-                bp=true;
-                ap=false;
-            }
+    //     while (a != 0 && b != 0) {
+    //         if (a - b >= 2 && !ap) {
+    //             sb.append("aa");
+    //             a-=2;
+    //             ap=true;
+    //             bp=false;
+    //         }
+    //         else if(b - a >= 2 && !bp){
+    //             sb.append("bb");
+    //             b-=2;
+    //             bp=true;
+    //             ap=false;
+    //         }
+    //         else if(a>b && !ap){
+    //             sb.append('a');
+    //             a--;
+    //             bp=true;
+    //             ap=false;
+    //         }
+    //         else if(b>a && !bp){
+    //             sb.append('b');
+    //             b--;
+    //             bp=true;
+    //             ap=false;
+    //         }
+    //         else if(!ap){
+    //             sb.append('a'); 
+    //             a--;
+    //             ap=true;
+    //             bp=false;
+    //         }
+    //         else{
+    //             sb.append('b');
+    //             b--;
+    //             bp=true;
+    //             ap=false;
+    //         }
+    //     }
+    //     while(b!=0){
+    //         sb.append('b');
+    //         b--;
+    //     }
+    //     while(a!=0){
+    //         sb.append('a');
+    //         a--;
+    //     }
+    //     return sb.toString();
+    // }
+    public String strWithout3a3b(int A, int B) {
+        StringBuilder res = new StringBuilder(A + B);
+        char a = 'a', b = 'b';
+        int i = A, j = B;
+        if (B > A) {
+            a = 'b';
+            b = 'a';
+            i = B;
+            j = A;
         }
-        while(b!=0){
-            sb.append('b');
-            b--;
+        while (i-- > 0) {
+            res.append(a);
+            if (i > j) {
+                res.append(a);
+                --i;
+            }
+            if (j-- > 0)
+                res.append(b);
         }
-        while(a!=0){
-            sb.append('a');
-            a--;
-        }
-        return sb.toString();
+        return res.toString();
     }
 }
