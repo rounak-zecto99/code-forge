677008aaa3d5d04ea9157332dc468a3abc4b0c6f
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-21 22:23:35 +0530

    Time: 31 ms (81.09%), Space: 60.6 MB (5.04%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,44 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        int n = nums.length;
         Arrays.sort(nums);
-        if(nums[0]>0)
-        return list;
-        
-        for(int i=0; i<n-2; i++){
-            if(i>0 && nums[i] == nums[i-1])
-            continue;
-
+        for(int i =0; i<nums.length-2;i++){
+            if(i>0 && nums[i]== nums[i-1]){
+                continue;
+            }
             int target = -nums[i];
             int start = i+1;
-            int end = n-1;
+            int end = nums.length-1;
 
             while(start<end){
-                if(nums[start]+nums[end] == target){
+                if(nums[start]+nums[end] ==  target){
                     List<Integer> row = new ArrayList<>();
-                    row.add(-target);
+                    row.add(-target); 
                     row.add(nums[start]);
                     row.add(nums[end]);
                     list.add(row);
                     start++;
                     end--;
-
-                    while(start<end && nums[start] == nums[start-1]){
-                        start++;
-                    }
-                    while(start<end && nums[end] == nums[end+1]){
-                        end--;
-                    }
-                }
-                else if(nums[start]+nums[end] < target){
+                  while (start < end && nums[start] == nums[start - 1]){
+                    start++;}
+                
+                while (start < end && nums[end] == nums[end + 1]){
+                     end--;}
+            }
+                else if(nums[start]+nums[end]< target){
                     start++;
                 }
                 else{
                     end--;
                 }
             }
-
         }
         return list;
     }
