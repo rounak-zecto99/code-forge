10563c8b965f76a8af4eef163644dd8888af7f28
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-24 03:24:53 +0530

    Time: 1 ms (71.85%), Space: 43.8 MB (15.28%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,57 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    static class Tuple{
-        int row;
-        TreeNode node;
-
-        Tuple(int row, TreeNode node){
-            this.row = row;
-            this.node = node;
-        }
-    }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        TreeMap<Integer,Integer> map = new TreeMap<>();
+        List<Integer> ans = new ArrayList<>();
 
-        if(root == null)
-        return list;
+        if (root == null)
+            return ans;
 
-        Queue<Tuple> q = new ArrayDeque<>();
-        q.offer(new Tuple(0,root));
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
 
-        while(!q.isEmpty()){
-            Tuple cur = q.poll();
+        while (!q.isEmpty()) {
+            int size = q.size();
 
-            TreeNode node = cur.node;
-            int row = cur.row;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
 
-            map.put(row,node.val);
+                if (i == size - 1)
+                    ans.add(node.val);
 
-            if(node.left != null){
-                q.offer(new Tuple(row+1,node.left));
-            }
-            if(node.right != null){
-                q.offer(new Tuple(row+1,node.right));
+                if (node.left != null)
+                    q.offer(node.left);
+
+                if (node.right != null)
+                    q.offer(node.right);
             }
         }
 
-        for(int val : map.values()){
-            list.add(val);
-        }
-        return list;
+        return ans;
     }
 }
