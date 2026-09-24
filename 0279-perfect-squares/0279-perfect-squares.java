90805e9ecdec20d9657bf98674dc604492ccd652
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-24 12:01:30 +0530

    Time: 115 ms (19.22%), Space: 69.7 MB (5.15%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,5 +1,6 @@
 class Solution {
     static int[] squares;
+    static int INF = 1000_000_000;
     static {
         squares = new int[100];
 
@@ -9,9 +10,13 @@ class Solution {
     }
 
     public int numSquares(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        int ans = helper(n,dp);
+        int[][]dp = new int[100][n+1];
+
+        for(int [] ar:dp){
+        Arrays.fill(ar,-1);
+        }
+
+        int ans = helper(n,99,dp);
 
         if(ans == Integer.MAX_VALUE)
         return -1;
@@ -19,25 +24,24 @@ class Solution {
         return ans;
     }
 
-    public int helper(int n, int[] dp) {
-        if (n == 0)
-            return 0;
-
-        if (n < 0) {
-            return Integer.MAX_VALUE;
-        }
-        if (dp[n] != -1)
-            return dp[n];
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < 100; i++) {
-            int res = helper(n - squares[i],dp);
-
-            if (res != Integer.MAX_VALUE) {
-                res = res + 1;
-                min = Math.min(res, min);
+    public int helper(int n,int index, int[][] dp) {
+        if(index == 0){
+            if(n%squares[0] == 0){
+                return n/squares[0];
+            }
+            else{
+                return INF;
             }
         }
-        return dp[n] = min;
+        if(dp[index][n] != -1)
+        return dp[index][n];
+
+        int notTake = helper(n,index-1,dp);
+        int Take = INF;
+
+        if(squares[index]<=n){
+            Take = 1+helper(n-squares[index],index,dp);
+        }
+        return dp[index][n] = Math.min(notTake,Take);
     }
 }
