f613a793e107a1e044285b03fd4ffd7290995bd8
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-27 00:29:06 +0530

    Time: 0 ms (100%), Space: 47.5 MB (36.95%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,38 +1,34 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) {
-            return null;
-        }
+        if(root == null)
+        return root;
 
-        if (key < root.val) {
-            root.left = deleteNode(root.left, key);
+        if(root.val > key){
+            root.left = deleteNode(root.left,key);
         }
-        else if (key > root.val) {
-            root.right = deleteNode(root.right, key);
+        else if(root.val < key){
+            root.right = deleteNode(root.right,key);
         }
-        else {
-            // Case 1: no left child
-            if (root.left == null) {
+        else{
+            if(root.left == null){
                 return root.right;
             }
-
-            // Case 2: no right child
-            if (root.right == null) {
+            if(root.right == null){
                 return root.left;
             }
+            TreeNode r = root.right;
+            TreeNode lastLeft = findlast(root.left); //connect to the rightmost of the left
+            lastLeft.right = r;
 
-            // Case 3: two children
-            TreeNode successor = root.right;
+            return root.left;
 
-            while (successor.left != null) {
-                successor = successor.left;
-            }
-
-            root.val = successor.val;
-
-            root.right = deleteNode(root.right, successor.val);
         }
-
+        return root;
+    }
+    TreeNode findlast(TreeNode root){
+        while(root.right != null){
+            root = root.right;
+        }
         return root;
     }
 }
