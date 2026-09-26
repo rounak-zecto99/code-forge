5d133c560264d8ac08a555a4ce285b5b09ebbd8e
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-26 13:22:09 +0530

    Time: 0 ms (100%), Space: 43 MB (70.65%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,31 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        helper(list,root);
-        return list;
-    }
-    public void helper(List<Integer> list, TreeNode root){
-        if(root == null)
-        return;
+        List<Integer> ans = new LinkedList<>();
+        TreeNode cur = root;
 
-        list.add(root.val);
-        helper(list,root.left);
-        helper(list,root.right);
+        while (cur != null) {
+            if (cur.left == null) {
+                ans.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left;
 
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+                if (prev.right == null) {
+                    prev.right = cur;
+                    ans.add(cur.val);
+                    cur = cur.left;
+                } else {
+                    prev.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+        return ans;
     }
 }
