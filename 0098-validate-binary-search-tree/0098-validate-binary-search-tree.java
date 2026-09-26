1acedf2966ca93c38671ffa86872294923ee4815
rounak-zecto99 <149151351+rounak-zecto99@users.noreply.github.com>
2026-09-27 02:17:18 +0530

    Time: 0 ms (100%), Space: 45.1 MB (65.56%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,14 +1,17 @@
 class Solution {
+    long prev = Long.MIN_VALUE;
     public boolean isValidBST(TreeNode root) {
-        return helper(root,Long.MAX_VALUE,Long.MIN_VALUE);
-    }
-    public boolean helper(TreeNode root, long max, long min){
         if(root == null)
         return true;
 
-        if(root.val>=max || root.val<=min)
+        if(!isValidBST(root.left))
         return false;
 
-        return helper(root.left,root.val,min) &&  helper(root.right,max,root.val); 
+        if(prev >= root.val)
+        return false;
+
+        prev = root.val;
+
+        return isValidBST(root.right);
     }
 }
